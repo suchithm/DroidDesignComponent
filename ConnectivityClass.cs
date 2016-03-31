ac6183e95a576d9fd47aa5c45e815585521b6be2
suchithm <suchimadavu@gmail.com>
2016-03-31 18:20:48 +0530

    Initial check-in of module DesignComponent

---
+++ ConnectivityClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.Net;
+using Android.Content;
+
+namespace DesignComponent
+{
+	public static class ConnectivityClass
+	{ 
+		public static bool isConnected(Context context)
+		{
+			ConnectivityManager connectivityManager = (ConnectivityManager) context.GetSystemService(Android.Content.Context.ConnectivityService);
+
+			NetworkInfo activeConnection = connectivityManager.ActiveNetworkInfo;
+			bool isOnline = (activeConnection != null) && activeConnection.IsConnected;
+			return isOnline;
+		}
+	}
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android .Support.V4;
+using Android.Support.Design;
+using Android.Support.Design.Widget; 
+using System;
+
+
+namespace DesignComponent
+{
+	[Activity (Label = "DesignComponent", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{ 
+		LinearLayout linearLayoutMain;
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+ 			SetContentView (Resource.Layout.Main);
+ 
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			linearLayoutMain = FindViewById<LinearLayout> (Resource.Id.linearLayoutMain);
+			button.Click += delegate {
+				if (!ConnectivityClass.isConnected (this))
+					fnSnackBar(GetString (Resource.String.no_internet),true);
+				else
+					fnSnackBar(GetString (Resource.String.connected_internet),false);
+ 			};  
+		}
+		void fnSnackBar( string strText,bool isLengthIndefinite)
+		{
+			Snackbar objSnackBar = Snackbar.Make (linearLayoutMain, strText, isLengthIndefinite ? Snackbar.LengthIndefinite : Snackbar.LengthShort)
+				.SetAction (Resource.String.retry, (v) => {
+				if (!ConnectivityClass.isConnected (this)) {
+					fnSnackBar (GetString (Resource.String.no_internet), true);
+				}
+			}); 
+			
+			//set  action button text color 
+			objSnackBar.SetActionTextColor(Android.Graphics.Color.Aqua); 
+	
+			Android.Views.View objView = objSnackBar.View; 
+			TextView txtAction = objView.FindViewById<TextView> (Resource.Id.snackbar_action);
+			txtAction.SetTextSize (Android.Util.ComplexUnitType.Dip, 18);
+
+			//set message text color
+			TextView txtMessage = objView.FindViewById<TextView> (Resource.Id.snackbar_text);
+			txtMessage.SetTextColor (Android.Graphics.Color.Red);
+			txtMessage.SetTextSize (Android.Util.ComplexUnitType.Dip, 18);
+     		objSnackBar.Show ();
+		}
+	}
+}
+
+
